02a6311b2a3a1433cac1de0cd2065777bda16cfa
rnair9 <117598568+rnair9@users.noreply.github.com>
2023-04-25 13:22:58 -0500

    Moved printBoard to separate file

--- main.asm
+++ main.asm
@@ -30,83 +30,35 @@ main:
     li $t2, 8 # number of columns
     li $t3, 6 # number of rows
     li $t5, 0 # current row
-    li $t6, 0
-    li $t7, 16
+    li $t6, 0 # binary value used to determine whether current row contains only spaces or dots and spaces
+    li $t7, 16 # total number of rows including both dotted rows and rows of spaces
 
     default_board:
         init_column:
-            li $t4, 0
-            beq $t6, $zero, init_row
-            row_space:
+            li $t4, 0 # initializing the counter for the number of columns
+            beq $t6, $zero, init_row # checking binary value if current row is supposed to have all spaces
+            row_space: # initializing a row of spaces
                 sb $s2, 0($t0)
                 addi $t0, $t0, 1
                 addi $t4, $t4, 1
                 blt $t4, $t7, row_space
-                li $t6, 0
+                li $t6, 0 # resetting the binary value to insert row of dots and spaces next
                 j init_column	
-            init_row:
+            init_row: # initializing a row of dots and spaces
                 sb $s3, 0($t0)
                 addi $t0, $t0, 1
-                beq $t4, $t2, last_column
+                beq $t4, $t2, last_column # not inserting an extra space when the last column is reached
                 addi $t4, $t4, 1
                 sb $s2, 0($t0)
                 addi $t0, $t0, 1
                 blt $t4, $t2, init_row
             last_column:
                 addi $t5, $t5, 1
-                li $t6, 1
-                blt $t5, $t3, init_column
+                li $t6, 1 # resetting the binary value to insert row of spaces next
+                blt $t5, $t3, init_column # branch if the 8x6 matrix is not complete yet
             
-                
-    li $t5, 0
-    addi $t0, $s0, 0
-    addi $t7, $s1, 0
-    li $t3, 11
-
-    print_board:
-        print_column:
-            li $t4, 0
-            lb $t8, 0($t7)
-            li $v0, 11
-            move $a0, $t8
-            syscall
-            li $v0, 4
-            la $a0, space
-            syscall
-            addi $t7, $t7, 1
-
-        print_row:
-            lb $t6, 0($t0)
-            li $v0, 11
-            move $a0, $t6
-            syscall
-            addi $t0, $t0, 1
-            beq $t2, $t4, end_row
-            addi $t4, $t4, 1
-            
-        
-            
-            lb $t6, 0($t0)
-            li $v0, 11
-            move $a0, $t6
-            syscall
-            addi $t0, $t0, 1
-            
-            li $v0, 4
-            la $a0, space
-            syscall
-            
-            
-            blt $t4, $t2, print_row
-            end_row:
-                li $v0, 4
-                la $a0, newline
-                syscall
-                addi $t5, $t5, 1
-                blt $t5, $t3, print_column
-                
-                
-        jal inputCheck
+        jal printBoard # subroutine to print the board                        
+        jal inputCheck # subroutine to check the user's input
         
      # $s6 subtract ascii of a
     addi $s6, $s6, -97
