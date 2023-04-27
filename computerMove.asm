954d754f47759f72aab1ad002f2bc6b9afaf662c
Shashank Vinay Kumar <shashankvinaykumar@gmail.com>
2023-04-27 13:18:46 -0500

    Add files via upload

--- computerMove.asm
+++ computerMove.asm
@@ -1,112 +1,46 @@
 .data
-rows: .word 8
-cols: .word 8
-board: .space 256
-validMoves: .space 240
-validMovesCount: .word 0
+random_seed: .word 1
 
 .text
 .globl computerMove
-
 computerMove:
-    jal findValidMoves 
 
+    # Generate a random row (letter between 'a' and 'k')
+    jal generateRandom
+    addi $t0, $v0, 'a'
 
-    lw $t0, validMovesCount
-    li $t1, 2
-    div $t0, $t1 
-    mflo $t0
-    jal generateRandomNumber
-    move $t1, $v0 
+    # Generate a random column (letter between 'a' and 'o')
+    jal generateRandom
+    addi $t1, $v0, 'a'
 
-    
-    sll $t1, $t1, 2
-    la $t2, validMoves
-    add $t2, $t2, $t1 
-    lw $s6, ($t2) 
-    addi $t2, $t2, 4 
-    lw $s7, ($t2)
+    # Call inputCheck to verify if it's a valid move
+    move $s6, $t0
+    move $s7, $t1
+    jal inputCheck
 
-    jr $ra 
+    # If the move is not valid, try another move
+    lb $t2, 0($s5)
+    beq $t2, $s2, computerMove
 
-findValidMoves:
-    lw $t0, rows
-    lw $t1, cols
+    # If the move is valid, update the board
+    jal checkForBoxes
+    beq $v0, 1, updateScore
 
-
-    li $t2, 1
-    li $t8, 0
-
-outer_loop2:
-
-    li $t3, 1
-
-inner_loop2:
-
-    move $a0, $t2
-    move $a1, $t3
-    jal checkForEmptySpace
-
-    beq $v0, 1, storeValidMove 
-
-
-    addi $t3, $t3, 2   # increment j
-    blt $t3, $t1, inner_loop2 
-
-
-    addi $t2, $t2, 2   
-    blt $t2, $t0, outer_loop2 
-
-
-    sw $t8, validMovesCount
+    # Return to main
     jr $ra
 
-storeValidMove:
-    la $t4, validMoves
-    sll $t5, $t8, 2
-    add $t4, $t4, $t5
-    sw $t2, ($t4)
-    addi $t4, $t4, 4
-    sw $t3, ($t4) # Store the column
+generateRandom:
+    # Generate a random integer between 0 and 15
+    lw $t0, random_seed
+    li $t1, 1103515245
+    li $t2, 12345
+    li $t3, 31
+    mul $t0, $t0, $t1
+    add $t0, $t0, $t2
+    and $t0, $t0, $t3
+    sw $t0, random_seed
+    jr $ra
 
-    addi $t8, $t8, 2
-    j inner_loop2
-
-checkForEmptySpace:
-    lw $t0, cols
-    lw $t1, rows
-    mult $a0, $t0
-mflo $t0
-add $t0, $t0, $a1
-sll $t0, $t0, 2 
-
-
-la $t1, board
-
-
-add $t1, $t1, $t0
-
-
-lw $t2, ($t1)
-seq $v0, $t2, 0
-jr $ra
-
-generateRandomNumber:
-li $v0, 0xABAD1DEA
-move $a0, $t0
-li $a1, 0
-li $a2, 0xFFFF
-
-
-loop1:
-    and $t0, $v0, $a2
-    srl $t0, $t0, 16
-    xor $v0, $v0, $t0
-
-    addiu $a1, $a1, 1
-    blt $a1, $a2, loop1
-
-
-div $v0, $a0
-mflo $v0
-jr $ra
+updateScore:
+    addi $s4, $s4, 1
+    jr $ra
